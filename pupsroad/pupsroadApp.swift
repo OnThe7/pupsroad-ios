237a8eac3fe41479002ccf1c54cfb260958a5368
KJH <kjhg5852@naver.com>
2022-07-15 09:19:38 +0900

    Initial Commit

---
+++ pupsroad/pupsroadApp.swift
@@ -0,0 +1,17 @@
+//
+//  pupsroadApp.swift
+//  pupsroad
+//
+//  Created by KJH on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct pupsroadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
