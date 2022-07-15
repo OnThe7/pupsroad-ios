aeee80d8a4923cb6b8d606f393616c13b84b542b
KJH <kjhg5852@naver.com>
2022-07-15 09:32:46 +0900

    feat : Int pod

---
+++ Pods/Target Support Files/Pods-pupsroad-pupsroadUITests/Pods-pupsroad-pupsroadUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pupsroad_pupsroadUITests : NSObject
+@end
+@implementation PodsDummy_Pods_pupsroad_pupsroadUITests
+@end
---
+++ Pods/Target Support Files/Pods-pupsroadTests/Pods-pupsroadTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pupsroadTests : NSObject
+@end
+@implementation PodsDummy_Pods_pupsroadTests
+@end
